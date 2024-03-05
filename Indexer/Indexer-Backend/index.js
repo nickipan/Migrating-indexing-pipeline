0e5ef0529d645f18170569c61446fc4faed31595
nickipan <57984917+nickipan@users.noreply.github.com>
2024-03-05 16:28:32 +0100

    Frontend

---
+++ Indexer/Indexer-Backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+const port = 5000
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+  }));
+
+app.use(express.json());
+
+const testData = {
+    status: '55',
+    duration: '10 min',
+    startTime: 'today',
+    completed: 'yes',
+    content: 'content'
+}
+
+app.get('/getIndexData', (req, res) => {
+    res.json(testData);
+});
+
+app.post('/sendIndexData', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(port, () => {
+    console.log("server is running")
+});
