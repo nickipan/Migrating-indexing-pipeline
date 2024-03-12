f5515e6796d11ec38bd0eddda56fd2add39de02b
norasilven <77102103+norasilven@users.noreply.github.com>
2024-03-12 21:09:39 +0100

    Using CORS for API server

--- API_server/swagger_server/__main__.py
+++ API_server/swagger_server/__main__.py
@@ -3,12 +3,25 @@
 import connexion
 
 from swagger_server import encoder
+from connexion.middleware import MiddlewarePosition
+from starlette.middleware.cors import CORSMiddleware
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
+
+    app.add_middleware(
+        CORSMiddleware,
+        position=MiddlewarePosition.BEFORE_EXCEPTION,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Indexing API'}, pythonic_params=True)
+    cors = CORS(app)
     app.run(port=8080)
 
 
