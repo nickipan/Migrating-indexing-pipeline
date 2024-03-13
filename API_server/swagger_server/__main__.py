1f5061f644e2cf51ed2ab943f26d9666120ebf6f
norasilven <77102103+norasilven@users.noreply.github.com>
2024-03-13 15:10:02 +0100

    enable cors attempt

--- API_server/swagger_server/__main__.py
+++ API_server/swagger_server/__main__.py
@@ -3,25 +3,20 @@
 import connexion
 
 from swagger_server import encoder
-from connexion.middleware import MiddlewarePosition
-from starlette.middleware.cors import CORSMiddleware
+# from connexion.middleware import MiddlewarePosition
+# from starlette.middleware.cors import CORSMiddleware
+from flask_cors import CORS
+from connexion.resolver import RestyResolver
 
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
 
-    app.add_middleware(
-        CORSMiddleware,
-        position=MiddlewarePosition.BEFORE_EXCEPTION,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    cors = CORS(app.app)
 
     app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Indexing API'}, pythonic_params=True)
-    cors = CORS(app)
+    app.add_api('swagger.yaml', resolver=RestyResolver('seaiceservices.api'), arguments={'title': 'Indexing API'}, pythonic_params=True)
+
     app.run(port=8080)
 
 
